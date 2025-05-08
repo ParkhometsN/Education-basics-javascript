1f913066f1287a7f6d9d15bfd7becc89d61be868
Paarxomets <parxomez1@mail.ru>
2025-05-08 11:47:01 +0300

    swithch

--- sc/secscr.js
+++ sc/secscr.js
@@ -65,8 +65,72 @@
 // } while (num <= 100 && num);
 
 
-for (let i = 2; i <= 10; i++){
-    if(i % 2){
-        alert(i)
-    };
-};
+// for (let i = 2; i <= 10; i++){
+//     if(i % 2){
+//         alert(i)
+//     };
+// };
+
+// button = document.getElementById('googlebutton')
+
+// const valu1 = 'n';
+
+// if (button === 'click'){
+//     alert('кнопка нажата')
+// }
+
+browser = prompt('choose browser', '');
+
+// switch (browser) {
+//     case 'Edge':
+//       alert( "You've got the Edge!" );
+//       break;
+  
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//       alert( 'Okay we support these browsers too' );
+//       break;
+  
+//     default:
+//       alert( 'We hope that this page looks ok!' );
+//   }
+
+// if (browser === 'Edge'){
+//     alert('You ve got the Edge!')
+// }else if (browser === 'Chrome' 
+//     || browser === 'Firefox' 
+//     || browser === 'Safari' 
+//     || browser === 'Opera'){
+//     alert( 'Okay we support these browsers too' );
+// }else{
+//     alert( 'We hope that this page looks ok!' );
+// }
+
+
+// let a = +prompt('a?', '');
+
+// if (a == 0) {
+//   alert( 0 );
+// }
+// if (a == 1) {
+//   alert( 1 );
+// }
+
+// if (a == 2 || a == 3) {
+//   alert( '2,3' );
+// }
+
+// switch (a){
+//     case 0:
+//         alert( 0 );
+//         break;
+//     case 1:
+//         alert( 1 );
+//         break;
+//     case 2:
+//     case 3:
+//         alert('2,3')
+//         break;
+// };
